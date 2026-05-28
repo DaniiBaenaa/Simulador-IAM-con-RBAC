b50b05a78a6bd0a03f394bbb157b04504843b026
DaniiBaenaa <daniel.baena@enti.cat>
2026-05-28 11:21:56 +0200

    comenzar el codigo

---
+++ source/sistemaa_IAM_principal.py
@@ -0,0 +1,6 @@
+#Este es nuestro programa principal del IAM con RBAC que queremos llevar a cabo.
+#Gestiona usuarios, roles y recursos
+#También verifica el acceso con herencia recursiva
+
+from entities import Entidad, Usuario, Rol, Recurso
+import time
