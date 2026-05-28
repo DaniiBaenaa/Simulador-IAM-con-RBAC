29c07507f51c73150be3b1dac54879adeca05a95
DaniiBaenaa <daniel.baena@enti.cat>
2026-05-28 11:26:56 +0200

    archivo para entidades

---
+++ source/entities.py
@@ -0,0 +1,5 @@
+#Aquí encontramos las clases base de nuestro sistema
+# la jerarquia que seguimos en POO es: entidad --> usuario / rol / recurso
+# para implementar polimorfismo en cada subclase implementamos su proprio describir() y tipo()
+
+from abc import ABC, abstractmethod
